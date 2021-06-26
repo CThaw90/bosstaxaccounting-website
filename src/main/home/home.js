d5d8299cd04612359edb334bb254c789e281b1ff
CThaw <christhaw90@gmail.com>
2021-06-26 10:25:36 -0400

    Completed contact us form interaction logic

--- src/main/home/home.js
+++ src/main/home/home.js
@@ -15,6 +15,40 @@ const scrollToElement = function (elementId) {
         window.scrollBy(0, dimensions.y);
     }
 };
+self.submitButtonVisible = ko.observable(true);
+self.name = {
+    value: ko.observable(),
+    error: ko.observable()
+};
+self.email = {
+    value: ko.observable(),
+    error: ko.observable()
+};
+self.services = {
+    options: [
+        'Tax services', 'Accounting', 'Credit services', 'Expungement services',
+        'Uncontested divorce filings', 'Bankruptcy petition preparation',
+        'Legal forms', 'Business'
+    ],
+    value: ko.observable(),
+    error: ko.observable()
+};
+self.name.value.subscribe(v => self.name.error(!v));
+self.email.value.subscribe(v => self.email.error(!v));
+self.services.value.subscribe(v => self.services.error(!v));
+
+self.submitContact = function () {
+    self.name.error(!self.name.value());
+    self.email.error(!self.email.value());
+    self.services.error(!self.services.value());
+
+    if (!self.name.error() && !self.email.error() && !self.services.error()) {
+        window.console.log(self.name.value());
+        window.console.log(self.email.value());
+        window.console.log(self.services.value());
+        self.submitButtonVisible(false);
+    }
+};
 
 self.navigateToHome = function () {
     scrollToElement('home-section');
