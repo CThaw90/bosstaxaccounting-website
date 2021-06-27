365e3723f7262e04dcd9be041d5da9ee4a54b5ab
CThaw <christhaw90@gmail.com>
2021-06-27 00:44:22 -0400

    Fixed a couple styling issues in About Us section and validating email formats used within Contact us form

--- src/main/home/home.js
+++ src/main/home/home.js
@@ -15,10 +15,11 @@ const FORM_STATE = {
     PENDING: 'pending',
     SUBMITTED: 'submitted'
 };
-const scrollToElement = function (elementId) {
+const scrollToElement = function (elementId, offset) {
     const dimensions = elements[elementId] ? elements[elementId].getBoundingClientRect() : null;
     if (dimensions) {
-        window.scrollBy(0, dimensions.y);
+        const y = dimensions.y - (offset ? offset + 10 : 0);
+        window.scrollBy(0, y);
     }
 };
 self.state = { current: ko.observable(FORM_STATE.IDLE) };
@@ -31,7 +32,8 @@ self.name = {
 };
 self.email = {
     value: ko.observable(),
-    error: ko.observable()
+    error: ko.observable(),
+    showErrorText: ko.observable(false)
 };
 self.service = {
     options: [
@@ -40,18 +42,23 @@ self.service = {
         'Legal forms', 'Business'
     ],
     value: ko.observable(),
-    error: ko.observable()
+    error: ko.observable(),
+
 };
 self.name.value.subscribe(v => self.name.error(!v));
-self.email.value.subscribe(v => self.email.error(!v));
+self.email.value.subscribe(v => {
+    self.email.error(!v);
+});
 self.service.value.subscribe(v => self.service.error(!v));
 
 self.submitContact = function () {
     self.name.error(!self.name.value());
-    self.email.error(!self.email.value());
+    self.email.showErrorText(!/.{2,}@.{2,}\..{2,}/.test(self.email.value()));
+    self.email.error(!self.email.value() || self.email.showErrorText());
     self.service.error(!self.service.value());
 
-    if (!self.name.error() && !self.email.error() && !self.service.error()) {
+
+    if (!self.name.error() && !self.email.error() && !self.service.error() && !self.email.showErrorText()) {
         self.state.current(FORM_STATE.PENDING)
         service.sendEmail({
             name: self.name.value(),
@@ -68,11 +75,11 @@ self.navigateToHome = function () {
 };
 
 self.navigateToServices = function () {
-    scrollToElement('services-section');
+    scrollToElement('services-section', 60);
 };
 
 self.navigateToAboutUs = function () {
-    scrollToElement('about-us-section');
+    scrollToElement('about-us-section', 10);
 };
 
 self.navigateToContactUs = function () {
